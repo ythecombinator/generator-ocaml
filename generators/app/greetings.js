8f54d16c547d7d03fa17de32d753029406cef47d
Matheus Brasil <matheus.brasil10@gmail.com>
2015-12-29 21:48:42 -0200

    Add a special greeting for users

---
+++ generators/app/greetings.js
@@ -0,0 +1,15 @@
+var chalk = require('chalk');
+
+var greetings = "" +
+    chalk.yellow("   ..  ..   / o._)                   .---.")      +
+    chalk.yellow("  /--'/--\  \-'||        .----.    .'     '.")    +
+    chalk.yellow(" /        \_/ / |      .'      '..'         '-.") +
+    chalk.yellow(".'\  \__\  __.'.'     .'          ì-._")          +
+    chalk.yellow(" )\ |  )\ |      _.'")                            +
+    chalk.yellow("// \\ // \\")                                     +
+    chalk.yellow("||_  \\|_  \\_")                                  +
+    chalk.yellow("'--' '--'' '--'")                                 +
+
+    chalk.gray("\n Yo OCaml! A Yeoman generator that provides a functional boilerplate to easily scaffold OCaml modules. \n");
+
+module.exports = greetings;
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,16 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+
+var yeoman     = require('yeoman-generator')
+    ,chalk     = require('chalk')
+    ,greetings = require('./greetings');
 
 module.exports = yeoman.generators.Base.extend({
+
   prompting: function () {
     var done = this.async();
 
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the wicked ' + chalk.red('generator-ocaml') + ' generator!'
-    ));
+    this.log(greetings)
 
     var prompts = [{
       type: 'confirm',
