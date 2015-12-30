75b97d08a45603fe92d4ff29944bb910edf7628f
Matheus Brasil <matheus.brasil10@gmail.com>
2015-12-29 23:26:11 -0200

    Add needed prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,12 +17,65 @@ module.exports = yeoman.generators.Base.extend({
     // Have Yeoman greet the user.
     this.log(greetings)
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var homeDir;
+    var osUserName;
+    var configFile;
+    var workingDirName = path.basename(process.cwd());
+
+    if (process.platform === 'win32') {
+      homeDir = process.env.USERPROFILE;
+      osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+    }
+
+    else {
+      homeDir = process.env.HOME || process.env.HOMEPATH;
+      osUserName = homeDir && homeDir.split('/').pop() || 'root';
+    }
+
+    var prompts = [
+      {
+        name: 'pkgName',
+        message: 'What\'s the package name?',
+        default: workingDirName
+      },
+      {
+        name: 'pkgDescription',
+        message: 'What\'s your package all about?'
+      },
+      {
+        name: 'pkgVersion',
+        message: 'What\'s the package version?',
+        default: '0.1.0'
+      },
+      {
+        name: 'authorName',
+        message: 'What\'s your name?',
+        default: 'Matheus'
+      },
+      {
+        name: 'authorEmail',
+        message: 'What\'s your email?',
+        default: 'matheus.brasil10@gmail.com'
+      },
+      {
+        name: 'userName',
+        message: 'What\'s your Github username?',
+        default: 'mabrasil'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license type',
+        choices: ['APACHE', 'BSD', 'GPLv3', 'MIT'],
+        default: 'MIT'
+      },
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Continue?',
+        default: false
+      }
+    ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
