ae1d897b5b9f74957047d07f98f0baa72557a530
Matheus Brasil <matheus.brasil10@gmail.com>
2015-12-30 23:10:37 -0200

    Set prompting info for tests

--- test/app.js
+++ test/app.js
@@ -1,10 +1,22 @@
 'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-generator').test;
+
+var path     = require('path')
+    ,assert  = require('yeoman-assert')
+    ,helpers = require('yeoman-generator').test
+    ,os      = require('os');
 
 describe('generator-ocaml:app', function () {
   before(function (done) {
+    var prompts = {
+      pkgName: 'an-awesome-ocaml-module',
+      pkgDescription: 'An awesome OCaml module.',
+      pkgVersion: '0.1.0',
+      authorName: 'Matheus Brasil',
+      authorEmail: 'matheus.brasil10@gmail.com',
+      userName: 'mabrasil',
+      license: 'MIT',
+      confirm: true
+    };
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({someOption: true})
       .withPrompts({someAnswer: true})
