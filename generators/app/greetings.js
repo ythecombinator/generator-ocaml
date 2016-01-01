df7713bd9887b973d2a39d67204ab4ef17628c7b
Matheus Brasil <matheus.brasil10@gmail.com>
2016-01-01 05:13:24 -0200

    Fix broken camel art and add a better message

--- generators/app/greetings.js
+++ generators/app/greetings.js
@@ -1,15 +1,21 @@
+'use strict';
+
 var chalk = require('chalk');
 
-var greetings = "" +
-    chalk.yellow("   ..  ..   / o._)                   .---.")      +
-    chalk.yellow("  /--'/--\  \-'||        .----.    .'     '.")    +
-    chalk.yellow(" /        \_/ / |      .'      '..'         '-.") +
-    chalk.yellow(".'\  \__\  __.'.'     .'          ì-._")          +
-    chalk.yellow(" )\ |  )\ |      _.'")                            +
-    chalk.yellow("// \\ // \\")                                     +
-    chalk.yellow("||_  \\|_  \\_")                                  +
-    chalk.yellow("'--' '--'' '--'")                                 +
+var greetings = '\n' +
+    chalk.yellow('                     ,,__ \n') +
+    chalk.yellow("           ..  ..   / o._)                   .---. \n") +
+    chalk.yellow("          /--'/--\  \-'||        .----.    .'     '. \n") +
+    chalk.yellow("         /        \_/ / |      .'      '..'         '-. \n") +
+    chalk.yellow("       .'\  \__\  __.'.'     .'          '-._ \n") +
+    chalk.yellow("         )\ |  )\ |      _.' \n") +
+    chalk.yellow('        // \\ // \\ \n') +
+    chalk.yellow("       ||_  \\|_  \\_ \n") +
+    chalk.yellow("       '--' '--'' '--' \n") +
 
-    chalk.gray("\n Yo OCaml! A Yeoman generator that provides a functional boilerplate to easily scaffold OCaml modules. \n");
+    chalk.yellow('\n Yo OCaml! ') +
+    chalk.gray('A Yeoman generator that provides a functional boilerplate to easily scaffold OCaml modules. \n') +
+    chalk.yellow("\n Let's talk about your new module? \n");
 
 module.exports = greetings;
+
